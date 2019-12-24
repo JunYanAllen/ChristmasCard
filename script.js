e1b8f339974ba016412cda01f53f9b50401232ab
zhangjunyan <olk022150663@gmail.com>
2019-12-24 10:32:33 +0800

    完成

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
         $('body').prepend('<div class="snow"></div>');
     }
 
-    var $countDate = new Date('Dec 25,2019 00:00:00').getTime();
+    var $countDate = new Date('Dec 24,2019 00:00:00').getTime();
+
+    var $now = new Date().getTime();
 
     function Christmas(){
         var $now = new Date().getTime();
@@ -25,10 +27,56 @@ $(document).ready(function(){
             $('#second').text($s);
     }
 
-    Christmas();
-    setInterval(function(){
+    if(($countDate - $now) < 0){
+        $('#day').text('0');
+        $('#hour').text('0');
+        $('#minute').text('0');
+        $('#second').text('0');
+
+        $('.content_div').animate({
+            left: "0px"
+        },2000);
+        $('.button_div').animate({
+            right: "45%"
+        },2000);
+    }else{
         Christmas();
+        setInterval(function(){
+            Christmas();
+        },1000);
+    }
+
+    setTimeout(function(){
+        $('text').each(function () {
+            const el = $(this);
+            const text = el.html().split('');
+            el.html(`<tspan>${text.join('</tspan><tspan>')}</tspan>`);
+        });
     },1000);
+    
+    $('.next-btn').click(function () {
+        var $TopPos = $('.content_div').scrollTop();
+        console.log($TopPos);    
+        $(".content_div").animate({
+            scrollTop: $TopPos + $(document).height()
+        }, 800);
+    });
+
+    $(".Merry").attr({
+        "y":($(document).height()/3),
+        "x":($(document).width()/2)
+    });
+    $(".Christmas").attr({
+        "y":($(document).height()/2),
+        "x":($(document).width()/2)
+    });
+    $(".for_you").attr({
+        "y":(($(document).height()/3)*2),
+        "x":($(document).width()/2)
+    });
+
+    $('.tag-container').css('height',$(document).heigt()-100);
+    $('.tag-3-side').css('height',$(document).height()-150);
 });
 
 
